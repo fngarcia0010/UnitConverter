3a309ee42babbf72553fc87350bb08e32b3ec099
Facundo Garcia <fngarcia0010@gmail.com>
2024-10-14 15:07:37 -0300

    Added: cors policy for development

--- UnitConverterAPI/UnitConverterAPI/Program.cs
+++ UnitConverterAPI/UnitConverterAPI/Program.cs
@@ -3,9 +3,16 @@ using UnitConverterAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
 builder.Services.AddScoped<UnitConverterService>();
 
 var app = builder.Build();
+
+app.UseCors("CorsPolicy");
 
 app.RegisterConvertUnitsEndpoints();
 
